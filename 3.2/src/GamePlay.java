98bddc9fddec8dd4522b9fe1cef5fec9a85c7002
ilan <ilankot76@gmail.com>
2026-01-08 19:44:08 +0200

    SD

---
+++ 3.2/src/GamePlay.java
@@ -0,0 +1,49 @@
+
+public class GamePlay implements Runnable {
+    private final Object lock = new Object();
+    private boolean Coin_avilable;
+    private int rounds_counter;
+
+    public void makeCoinAvail(boolean val) {
+        if (val) {
+            Coin_avilable = true;
+            notifyAll();
+        } else
+            Coin_avilable = false;
+    }
+
+    @Override
+    public void run() {
+    }
+
+    public boolean coinflip() {
+        synchronized (lock) {
+            while (!Coin_avilable) {
+                System.out.println("Thread " + Thread.currentThread().getName() + " waiting for coin to be available.");
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+
+            System.out.println("Thread " + Thread.currentThread().getName() + "flipping the coin.");
+            Coin_avilable = false;
+            rounds_counter++;
+
+            int flip = (int) (Math.random() * 2);
+            boolean result = (flip == 0) ? false : true;
+
+            Coin_avilable = true;
+            lock.notifyAll();
+            return result;
+
+        }
+
+    }
+
+    public int getnumberofrounds() {
+        return rounds_counter;
+    }
+
+}
