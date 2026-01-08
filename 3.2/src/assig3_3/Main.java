51d71050d877d85d08760b9f21d7de0bd4bc66b5
ilan <ilankot76@gmail.com>
2026-01-08 21:50:00 +0200

    main

--- 3.2/src/assig3_3/Main.java
+++ 3.2/src/assig3_3/Main.java
@@ -10,8 +10,28 @@ public class Main {
 		final int numOfSaladsToPrepare = scan.nextInt();
 		System.out.println("Preparing " + numOfSaladsToPrepare + " Salads...");
 
-		// YOUR CODE HERE: use threads to prepare N salads (as the user requested)
-		
+		SlicerMachine machine = new SlicerMachine(numOfSaladsToPrepare);
+
+		Thread cucumbersThread = new CucumbersThread(machine);
+		Thread tomatoesThread = new TomatoesThread(machine);
+		Thread onionThread = new OnionThread(machine);
+		Thread slicerThread = new SlicerThread(machine);
+
+		cucumbersThread.start();
+		tomatoesThread.start();
+		onionThread.start();
+		slicerThread.start();
+
+		try {
+			// Wait for the slicer to reach the target count (it sets machine to done)
+			slicerThread.join();
+			cucumbersThread.join();
+			tomatoesThread.join();
+			onionThread.join();
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+
 		System.out.println("Done");
 		scan.close();
 	}
