d0e8b7d94d6880576065d6eb7bc4ad127b321e49
ilan <ilankot76@gmail.com>
2026-01-08 21:47:39 +0200

    TOMATO

--- 3.2/src/assig3_3/TomatoesThread.java
+++ 3.2/src/assig3_3/TomatoesThread.java
@@ -2,4 +2,21 @@ package assig3_3;
 
 public class TomatoesThread extends Thread {
 
+    private final SlicerMachine machine;
+
+    public TomatoesThread(SlicerMachine machine) {
+        super("TomatoesThread");
+        this.machine = machine;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!machine.isDone()) {
+                machine.addOneTomato();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
